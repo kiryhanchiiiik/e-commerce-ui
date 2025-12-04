5e124fbbe3d5403cdb14146d91b6f996e2ea7fed
kiryhanchiiiik <kirillsabelkijk@gmail.com>
2025-12-04 16:19:48 +0200

    Add price and cart button to ProductsCard

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { ProductType } from "@/types";
+import { ShoppingCart } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -53,6 +54,14 @@ const ProductCart = ({ product }: { product: ProductType }) => {
               ))}
             </div>
           </div>
+        </div>
+        {/* PRICE AND CART BTN */}
+        <div className="flex items-center justify-between">
+          <p className="font-medium">${product.price.toFixed(2)}</p>
+          <button className="flex items-center gap-2 ring ring-gray-200 shadow-lg rounded-md px-2 py-1 text-sm cursor-pointer hover:text-white hover:bg-black transition-all duration-300">
+            <ShoppingCart className="w-4 h-4" />
+            Add to Cart
+          </button>
         </div>
       </div>
     </div>
