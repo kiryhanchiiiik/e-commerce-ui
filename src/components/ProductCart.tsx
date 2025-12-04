5aedab910c5fe32819ef4ada1927933530c54d13
kiryhanchiiiik <kirillsabelkijk@gmail.com>
2025-12-04 12:02:53 +0200

    Add ProductsList component to HomePage

---
+++ src/components/ProductCart.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import { ProductType } from "@/types";
+
+const ProductCart = ({ product }: { product: ProductType }) => {
+  return <div>ProductCart</div>;
+};
+export default ProductCart;
