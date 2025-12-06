7724e74e36eabb32436a973738b2682829e956c6
kiryhanchiiiik <kirillsabelkijk@gmail.com>
2025-12-06 12:35:05 +0200

    add handleFilter to Filter component

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -6,6 +6,12 @@ const Filter = () => {
   const searchParams = useSearchParams();
   const router = useRouter();
   const pathName = usePathname();
+
+  const handlFilter = (value: string) => {
+    const params = new URLSearchParams(searchParams);
+    params.set("sort", value);
+    router.push(`${pathName}?${params.toString()}`, { scroll: false });
+  };
   return (
     <div className="flex items-center justify-end gap-2 text-sm text-gray-500 my-6">
       <span>Sort by:</span>
@@ -13,6 +19,7 @@ const Filter = () => {
         name="sort"
         id="sort"
         className="ring-1 ring-gray-200 shadow-md p-1 rounded-sm"
+        onChange={(e) => handlFilter(e.target.value)}
       >
         <option value="newest">Newest</option>
         <option value="oldest">Oldest</option>
