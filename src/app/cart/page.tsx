0d31613965684a80cc2d5eb8d3dea10bbdfaeda0
kiryhanchiiiik <kirillsabelkijk@gmail.com>
2025-12-06 13:26:59 +0200

    add Cart Details

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { CartItemsType } from "@/types";
+import { ArrowRight } from "lucide-react";
 import { useRouter, useSearchParams } from "next/navigation";
 
 const steps = [
@@ -108,6 +109,50 @@ const CartPage = () => {
             </p>
           </div>
         ))}
+      </div>
+      {/* STEPS AND DETAILS */}
+      <div className="w-full flex flex-col lg:flex-row gap-16">
+        {/* STEPS */}
+        <div className="w-full lg:w-7/12 shadow-lg border-1 border-gray-100 p-8 rounded-lg flex flex-col gap-8">
+          1
+        </div>
+        {/* DETAILS */}
+        <div className="w-full lg:w-5/12 shadow-lg border-1 border-gray-100 p-8 rounded-lg flex flex-col gap-8">
+          <h2 className="font-semibold">Cart Details</h2>
+          <div className="flex flex-col gap-4">
+            <div className="flex justify-between text-sm">
+              <p className="text-gray-500">Subtotal</p>
+              <p className="font-medium">
+                $
+                {cartItems
+                  .reduce((acc, item) => acc + item.price * item.quantity, 0)
+                  .toFixed(2)}
+              </p>
+            </div>
+            <div className="flex justify-between text-sm">
+              <p className="text-gray-500">Discount(10%)</p>
+              <p className="font-medium">$10</p>
+            </div>
+            <div className="flex justify-between text-sm">
+              <p className="text-gray-500">Shipping Fee</p>
+              <p className="font-medium">$10</p>
+            </div>
+            <hr className="border-gray-200" />
+            <div className="flex justify-between">
+              <p className="text-gray-800 font-semibold">Total</p>
+              <p className="font-medium">
+                ${" "}
+                {cartItems
+                  .reduce((acc, item) => acc + item.price * item.quantity, 0)
+                  .toFixed(2)}
+              </p>
+            </div>
+          </div>
+          <button className="w-full bg-gray-800 hover:bg-gray-900 transition-all duration-300 text-white p-2 rounded-lg cursor-pointer flex items-center justify-center gap-2">
+            Continue
+            <ArrowRight className="w-3 h-3" />
+          </button>
+        </div>
       </div>
     </div>
   );
