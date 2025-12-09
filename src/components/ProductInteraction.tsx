0421f89c601c0119e45ce3649a777703ebfae948
kiryhanchiiiik <kirillsabelkijk@gmail.com>
2025-12-09 10:23:24 +0200

    add quantity to id page

--- src/components/ProductInteraction.tsx
+++ src/components/ProductInteraction.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import { ProductType } from "@/types";
+import { Minus, Plus } from "lucide-react";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useState } from "react";
 
 const ProductInteraction = ({
   product,
@@ -15,10 +17,21 @@ const ProductInteraction = ({
   const router = useRouter();
   const pathname = usePathname();
   const searchParams = useSearchParams();
+  const [quantity, setQuantity] = useState(1);
+
   const handleTypeChange = (type: string, value: string) => {
     const params = new URLSearchParams(searchParams.toString());
     params.set(type, value);
     router.push(`${pathname}?${params.toString()}`, { scroll: false });
+  };
+  const handleQuantityChange = (type: "increment" | "decrement") => {
+    if (type === "increment") {
+      setQuantity((prev) => prev + 1);
+    } else {
+      if (quantity > 1) {
+        setQuantity((prev) => prev - 1);
+      }
+    }
   };
 
   return (
@@ -67,7 +80,24 @@ const ProductInteraction = ({
         </div>
       </div>
       {/* QUANTITY */}
-      <div className="flex flex-col gap-2 text-sm"></div>
+      <div className="flex flex-col gap-2 text-sm">
+        <span className="text-gray-500">Quantity</span>
+        <div className="flex items-center gap-2">
+          <button
+            className="cursor-pointer border-1 border-gray-300 p-1"
+            onClick={() => handleQuantityChange("decrement")}
+          >
+            <Minus className="w-4 h-4" />
+          </button>
+          <span>{quantity}</span>
+          <button
+            className="cursor-pointer border-1 border-gray-300 p-1"
+            onClick={() => handleQuantityChange("increment")}
+          >
+            <Plus className="w-4 h-4" />
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
