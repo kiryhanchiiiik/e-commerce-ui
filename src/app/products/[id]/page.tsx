6f3793b2468a04d3e43f34a3c9971b2ad2b9ad1b
kiryhanchiiiik <kirillsabelkijk@gmail.com>
2025-12-09 09:50:40 +0200

    add Image to id page

---
+++ src/app/products/[id]/page.tsx
@@ -0,0 +1,49 @@
+import { ProductType } from "@/types";
+import Image from "next/image";
+
+const product: ProductType = {
+  id: 1,
+  name: "Adidas CoreFit T-Shirt",
+  shortDescription:
+    "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
+  description:
+    "Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit. Lorem ipsum dolor sit amet consect adipisicing elit lorem ipsum dolor sit.",
+  price: 59.9,
+  sizes: ["xs", "s", "m", "l", "xl"],
+  colors: ["gray", "purple", "green"],
+  images: {
+    gray: "/products/1g.png",
+    purple: "/products/1p.png",
+    green: "/products/1gr.png",
+  },
+};
+
+const ProductPage = async ({
+  params,
+  searchParams,
+}: {
+  params: Promise<{ id: string }>;
+  searchParams: Promise<{ color: string; size: string }>;
+}) => {
+  const { size, color } = await searchParams;
+
+  const selectedSize = size || (product.sizes[0] as string);
+  const selectedColor = color || (product.colors[0] as string);
+
+  return (
+    <div className="flex flex-col gap-4 lg:flex-row md:gap-12 mt-12">
+      {/* IMAGE */}
+      <div className="w-full lg:w-5/12 relative aspect-[2/3]">
+        <Image
+          src={product.images[selectedColor]}
+          alt={product.name}
+          fill
+          className="object-contain rounded-md"
+        />
+      </div>
+      {/* DETAILS */}
+      <div className="w-full lg:w-7/12"></div>
+    </div>
+  );
+};
+export default ProductPage;
